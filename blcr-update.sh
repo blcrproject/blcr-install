5a4d105af077e44ddd15b8b8d2e80582b9eb67ee
BlacerCoin <46682320+blcrproject@users.noreply.github.com>
2019-01-27 22:33:02 +0300

    Update blcr-update.sh

--- blcr-update.sh
+++ blcr-update.sh
@@ -45,6 +45,8 @@ function compile_node() {
   sleep 7
   
   echo -e "Remove the old $COIN_NAME wallet from the system"
+  rm -rf $CONFIGFOLDER/blocks/ >/dev/null 2>&1
+  rm -rf $CONFIGFOLDER/chainstate/ >/dev/null 2>&1
   rm -f /usr/local/bin/blacercoin* >/dev/null 2>&1
   rm $CONFIGFOLDER/banlist.dat >/dev/null 2>&1
   rm $CONFIGFOLDER/mnpayments.dat >/dev/null 2>&1
